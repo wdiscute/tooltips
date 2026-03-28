81e1a42859eef43ffe8838f3202c9d3f297cfdb8
wd <lucasalvesbispo2@gmail.com>
2026-03-28 20:31:36 +0000

    updated to use tyson's text lib

--- src/main/java/com/wdiscute/libtooltips/Tooltips.java
+++ src/main/java/com/wdiscute/libtooltips/Tooltips.java
@@ -1,5 +1,6 @@
 package com.wdiscute.libtooltips;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
@@ -15,6 +16,11 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.player.TradeWithVillagerEvent;
+import net.tysontheember.emberstextapi.EmbersTextAPI;
+import net.tysontheember.emberstextapi.immersivemessages.api.ImmersiveMessage;
+import net.tysontheember.emberstextapi.immersivemessages.api.MarkupParser;
+import net.tysontheember.emberstextapi.immersivemessages.api.TextSpan;
 
 import java.util.List;
 
@@ -22,18 +28,11 @@ import java.util.List;
 public class Tooltips
 {
     public static final String MOD_ID = "libtooltips";
-    public static float hue;
 
 
     public Tooltips(IEventBus modEventBus, ModContainer modContainer)
     {
         NeoForge.EVENT_BUS.addListener(Tooltips::modifyItemTooltip);
-        NeoForge.EVENT_BUS.addListener(Tooltips::renderFrame);
-    }
-
-    public static void renderFrame(RenderFrameEvent.Post event)
-    {
-        Tooltips.hue += 0.001f * event.getPartialTick().getRealtimeDeltaTicks() * 5;
     }
 
     public static void modifyItemTooltip(ItemTooltipEvent event)
@@ -47,19 +46,13 @@ public class Tooltips
         String baseTooltip = "tooltip." + namespace + "." + path;
         String baseTooltipNoShift = "tooltip.always." + namespace + "." + path;
 
-        if (I18n.exists(baseTooltip + ".name"))
-        {
-            tooltipComponents.removeFirst();
-            tooltipComponents.addFirst(decodeTranslationKey(baseTooltip + ".name"));
-        }
-
         if (I18n.exists(baseTooltipNoShift + ".0"))
         {
             for (int i = 0; i < 100; i++)
             {
                 if (!I18n.exists(baseTooltipNoShift + "." + i))
                     break;
-                tooltipComponents.add(decodeTranslationKey(baseTooltipNoShift + "." + i));
+                tooltipComponents.add(Component.translatable(baseTooltipNoShift + "." + i));
             }
         }
 
@@ -74,163 +67,12 @@ public class Tooltips
                 {
                     if (!I18n.exists(baseTooltip + "." + i))
                         break;
-                    tooltipComponents.add(decodeTranslationKey(baseTooltip + "." + i));
+                    tooltipComponents.add(Component.translatable(baseTooltip + "." + i));
                 }
             } else
             {
                 tooltipComponents.add(Component.translatable("tooltip.libtooltips.generic.shift_up"));
             }
         }
-    }
-
-
-    public static Component decodeTranslationKey(String s)
-    {
-        return decodeString(I18n.get(s));
-    }
-
-    public static Component decodeString(String s)
-    {
-        MutableComponent component = Component.empty();
-
-        //transform all <rgb> and <gradient> into it's corresponding things
-        for (int i = 0; i < 100; i++)
-        {
-            if (s.indexOf("<rgb>") < s.indexOf("<gradient-"))
-            {
-                if (s.contains("<rgb>") && s.contains("</rgb>"))
-                {
-                    component.append(Component.literal(s.substring(0, s.indexOf("<rgb>"))));
-                    component.append(Tooltips.RGBEachLetter(s.substring(s.indexOf("<rgb>") + 5, s.indexOf("</rgb>"))));
-                    s = s.substring(s.indexOf("</rgb>") + 6);
-                    continue;
-                }
-            }
-
-            if (s.indexOf("<gradient-") < s.indexOf("<rgb>"))
-            {
-                if (s.contains("<gradient-") && s.contains("</gradient-"))
-                {
-                    float min = Float.parseFloat("0." + s.substring(s.indexOf("<gradient") + 10, s.indexOf("<gradient") + 12));
-                    float max = Float.parseFloat("0." + s.substring(s.indexOf("</gradient") + 11, s.indexOf("</gradient") + 13));
-
-                    component.append(Component.literal(s.substring(0, s.indexOf("<gradient"))));
-                    component.append(Tooltips.gradient(s.substring(s.indexOf("<gradient") + 13, s.indexOf("</gradient")), min, max));
-                    s = s.substring(s.indexOf("</gradient-") + 14);
-                    continue;
-                }
-            }
-
-            if (s.contains("<rgb>") && s.contains("</rgb>"))
-            {
-                component.append(Component.literal(s.substring(0, s.indexOf("<rgb>"))));
-                component.append(Tooltips.RGBEachLetter(s.substring(s.indexOf("<rgb>") + 5, s.indexOf("</rgb>"))));
-                s = s.substring(s.indexOf("</rgb>") + 6);
-                continue;
-            }
-
-            if (s.contains("<gradient-") && s.contains("</gradient-"))
-            {
-                float min = Float.parseFloat("0." + s.substring(s.indexOf("<gradient") + 10, s.indexOf("<gradient") + 12));
-                float max = Float.parseFloat("0." + s.substring(s.indexOf("</gradient") + 11, s.indexOf("</gradient") + 13));
-
-                component.append(Component.literal(s.substring(0, s.indexOf("<gradient"))));
-                component.append(Tooltips.gradient(s.substring(s.indexOf("<gradient") + 13, s.indexOf("</gradient")), min, max));
-                s = s.substring(s.indexOf("</gradient-") + 14);
-                continue;
-            }
-
-            component.append(s);
-            break;
-
-        }
-
-        return component;
-    }
-
-    public static Component gradient(String text, float min, float max)
-    {
-        MutableComponent c = Component.empty();
-
-        for (int i = 0; i < text.length(); i++)
-        {
-            String s = text.substring(i, i + 1);
-            float pingPongedHue = mapHuePingPong(i * 0.01f + hue, min, max);
-            int color = hueToRGBInt(pingPongedHue);
-            Component l = Component.literal(s).withColor(color);
-            c.append(l);
-        }
-
-        return c;
-    }
-
-    public static float mapHuePingPong(float h, float min, float max)
-    {
-        float t = Math.abs(2f * (h % 1) - 1f);
-        return min + t * (max - min);
-    }
-
-    public static Component RGBEachLetter(String text)
-    {
-        return RGBEachLetter(text, 0.01f);
-    }
-
-    public static Component RGBEachLetter(String text, float speed)
-    {
-        MutableComponent c = Component.empty();
-
-        for (int i = 0; i < text.length(); i++)
-        {
-            String s = text.substring(i, i + 1);
-
-            int color = hueToRGBInt(i * speed + hue);
-
-            Component l = Component.literal(s).withColor(color);
-
-            c.append(l);
-        }
-
-        return c;
-    }
-
-
-    public static int hueToRGBInt(float hue)
-    {
-        int r = 0, g = 0, b = 0;
-
-        float h = (hue - (float) Math.floor(hue)) * 6.0f;
-        float f = h - (float) Math.floor(h);
-        float q = 1.0f - f;
-        float t = 1.0f - (1.0f - f);
-        switch ((int) h)
-        {
-            case 0:
-                r = (int) (255.0f + 0.5f);
-                g = (int) (t * 255.0f + 0.5f);
-                break;
-            case 1:
-                r = (int) (q * 255.0f + 0.5f);
-                g = (int) (255.0f + 0.5f);
-                break;
-            case 2:
-                g = (int) (255.0f + 0.5f);
-                b = (int) (t * 255.0f + 0.5f);
-                break;
-            case 3:
-                g = (int) (q * 255.0f + 0.5f);
-                b = (int) (255.0f + 0.5f);
-                break;
-            case 4:
-                r = (int) (t * 255.0f + 0.5f);
-                g = 0;
-                b = (int) (255.0f + 0.5f);
-                break;
-            case 5:
-                r = (int) (255.0f + 0.5f);
-                g = 0;
-                b = (int) (q * 255.0f + 0.5f);
-                break;
-        }
-        return 0xff000000 | (r << 16) | (g << 8) | (b);
     }
 }
