6eba0deecc8d684750a93a4b9c2253f36f72f09f
wd <lucasalvesbispo2@gmail.com>
2026-07-03 15:54:35 +0100

    v2.3 release, added data component and interface support

--- src/main/java/com/wdiscute/libtooltips/Tooltips.java
+++ src/main/java/com/wdiscute/libtooltips/Tooltips.java
@@ -1,9 +1,12 @@
 package com.wdiscute.libtooltips;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import com.mojang.serialization.Codec;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.component.DataComponentType;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
@@ -14,7 +17,6 @@ import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-import net.neoforged.neoforge.client.settings.IKeyConflictContext;
 import net.neoforged.neoforge.common.ModConfigSpec;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -23,6 +25,8 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
 import org.apache.commons.lang3.function.TriFunction;
 import org.lwjgl.glfw.GLFW;
 
@@ -38,8 +42,9 @@ public class Tooltips
 {
     public static final String MOD_ID = "libtooltips";
 
-    public Tooltips(IEventBus modEventBus, ModContainer modContainer)
+    public Tooltips(IEventBus eventBus, ModContainer modContainer)
     {
+        DATA_COMPONENT_TYPES.register(eventBus);
         NeoForge.EVENT_BUS.addListener(Tooltips::modifyItemTooltip);
         modContainer.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
     }
@@ -66,6 +71,29 @@ public class Tooltips
         }
     }
 
+    /**
+     * Implement this interface if you wish your item to have tooltips without requiring translation keys or data components
+     */
+    public interface ItemTooltip
+    {
+        List<String> getAlwaysTooltips(ItemStack stack);
+
+        List<String> getShiftTooltips(ItemStack stack);
+    }
+
+    /**
+     * Adding these data components to an ItemStack will display the tooltips
+     */
+    public static final DeferredRegister<DataComponentType<?>> DATA_COMPONENT_TYPES = DeferredRegister.createDataComponents(Registries.DATA_COMPONENT_TYPE, Tooltips.MOD_ID);
+
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<List<String>>> TOOLTIP_SHIFT = DATA_COMPONENT_TYPES
+            .register("tooltip_shift", () -> new DataComponentType.Builder<List<String>>().persistent(Codec.STRING.listOf()).build());
+
+    public static final DeferredHolder<DataComponentType<?>, DataComponentType<List<String>>> TOOLTIP_ALWAYS = DATA_COMPONENT_TYPES
+            .register("tooltip_always", () -> new DataComponentType.Builder<List<String>>().persistent(Codec.STRING.listOf()).build());
+
+
+
 
     private static final Pattern TAG_PATTERN = Pattern.compile("<([a-zA-Z0-9_]+)>(.*?)</\\1>", Pattern.DOTALL);
 
@@ -79,7 +107,6 @@ public class Tooltips
     public static boolean hasTags(String input)
     {
         Matcher matcher = TAG_PATTERN.matcher(input);
-
 
         while (matcher.find())
         {
@@ -108,6 +135,10 @@ public class Tooltips
         return resolveTagsToComponent(input, null, null);
     }
 
+    /**
+     * Resolves the input string into its tags and passes each tag to the respective processor.
+     * Returns a MutableComponent with all the tags resolved into subcomponents
+     */
     public static MutableComponent resolveTagsToComponent(String input, @Nullable ItemStack stack, @Nullable Entity entity)
     {
         MutableComponent result = Component.empty();
@@ -141,8 +172,13 @@ public class Tooltips
         return result;
     }
 
+    /**
+     * Handles all the logic related to applying tooltips to the description of an ItemStack when hovering it.
+     */
     public static void modifyItemTooltip(ItemTooltipEvent event)
     {
+        if (!Config.ENABLE_TOOLTIPS.get()) return;
+
         List<Component> tooltipComponents = event.getToolTip();
         ItemStack stack = event.getItemStack();
 
@@ -153,19 +189,52 @@ public class Tooltips
         String baseTooltipNoShift = "tooltip.always." + namespace + "." + path;
         StringBuilder spaces = new StringBuilder().repeat(" ", Config.SPACES_BEFORE_TOOLTIP.get());
 
-        if (I18n.exists(baseTooltipNoShift + ".0"))
+        List<String> compAlways = stack.getOrDefault(TOOLTIP_ALWAYS, List.of());
+        List<String> compShift = stack.getOrDefault(TOOLTIP_SHIFT, List.of());
+
+        List<String> interfaceShift;
+        List<String> interfaceAlways;
+        if (stack.getItem() instanceof ItemTooltip it)
         {
+            interfaceShift = it.getShiftTooltips(stack);
+            interfaceAlways = it.getAlwaysTooltips(stack);
+        }
+        else
+        {
+            interfaceShift = List.of();
+            interfaceAlways = List.of();
+        }
+
+        //always tooltips
+        if (I18n.exists(baseTooltipNoShift + ".0") || !compAlways.isEmpty() || !interfaceAlways.isEmpty())
+        {
+            //add tooltips from data component
+            for (String string : compAlways)
+                tooltipComponents.add(Component.literal(spaces.toString())
+                        .append(resolveTagsToComponentFromTranslationKey(string, stack, event.getEntity()).withStyle(Style.EMPTY.withColor(Config.DEFAULT_COLOR.getAsInt()))));
+
+            //add tooltips from interface
+            if (stack.getItem() instanceof ItemTooltip it)
+                for (String string : it.getAlwaysTooltips(stack))
+                    tooltipComponents.add(Component.literal(spaces.toString()).append(resolveTagsToComponentFromTranslationKey(string, stack, event.getEntity()).withStyle(Style.EMPTY.withColor(Config.DEFAULT_COLOR.getAsInt()))));
+
+            //add tooltips from translation keys
             for (int i = 0; i < 100; i++)
             {
+                //break if tooltip doesn't exist
                 if (!I18n.exists(baseTooltipNoShift + "." + i))
                     break;
+
+                //break if tooltip is "hide"
                 if (I18n.get(baseTooltipNoShift + "." + i).equals("hide"))
                     break;
+
                 tooltipComponents.add(Component.literal(spaces.toString()).append(resolveTagsToComponentFromTranslationKey(baseTooltipNoShift + "." + i, stack, event.getEntity()).withStyle(Style.EMPTY.withColor(Config.DEFAULT_COLOR.getAsInt()))));
             }
         }
 
-        if (I18n.exists(baseTooltip + ".0"))
+        //shift tooltips
+        if (I18n.exists(baseTooltip + ".0") || !compShift.isEmpty() || !interfaceShift.isEmpty())
         {
             boolean shift = InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), Client.EXPAND.getKey().getValue());
             if (shift)
@@ -174,19 +243,33 @@ public class Tooltips
                 if (Config.LINE_BEFORE.getAsBoolean())
                     tooltipComponents.add(Component.translatable("tooltip.libtooltips.generic.empty"));
 
+                //add tooltips from data component
+                for (String string : compShift)
+                    tooltipComponents.add(Component.literal(spaces.toString())
+                            .append(resolveTagsToComponentFromTranslationKey(string, stack, event.getEntity()).withStyle(Style.EMPTY.withColor(Config.DEFAULT_COLOR.getAsInt()))));
+
+                //add tooltips from interface
+                if (stack.getItem() instanceof ItemTooltip it)
+                    for (String string : it.getShiftTooltips(stack))
+                        tooltipComponents.add(Component.literal(spaces.toString()).append(resolveTagsToComponentFromTranslationKey(string, stack, event.getEntity()).withStyle(Style.EMPTY.withColor(Config.DEFAULT_COLOR.getAsInt()))));
+
+                //add tooltips from translation keys
                 for (int i = 0; i < 100; i++)
                 {
+                    //break if tooltip doesn't exist
                     if (!I18n.exists(baseTooltip + "." + i))
                         break;
+
+                    //break if tooltip is "hide"
                     if (I18n.get(baseTooltip + "." + i).equals("hide"))
                         break;
+
                     tooltipComponents.add(Component.literal(spaces.toString()).append(
                             resolveTagsToComponentFromTranslationKey(baseTooltip + "." + i, stack, event.getEntity()).withStyle(Style.EMPTY.withColor(Config.DEFAULT_COLOR.getAsInt()))));
                 }
 
                 if (Config.LINE_AFTER.getAsBoolean())
                     tooltipComponents.add(Component.translatable("tooltip.libtooltips.generic.empty"));
-
             }
             else
             {
@@ -198,6 +281,10 @@ public class Tooltips
     public static class Config
     {
         private static final ModConfigSpec.Builder BUILDER_CLIENT = new ModConfigSpec.Builder();
+
+        public static final ModConfigSpec.BooleanValue ENABLE_TOOLTIPS = BUILDER_CLIENT
+                .translation("libtooltips.configuration.disable_tooltips")
+                .define("disable_tooltips", true);
 
         public static final ModConfigSpec.BooleanValue LINE_BEFORE = BUILDER_CLIENT
                 .translation("libtooltips.configuration.line_before")
@@ -215,8 +302,6 @@ public class Tooltips
                 .translation("libtooltips.configuration.spaces_before_tooltip")
                 .defineInRange("spaces_before_tooltip", 2, 0, 10);
 
-
         static final ModConfigSpec SPEC = BUILDER_CLIENT.build();
-
     }
 }
